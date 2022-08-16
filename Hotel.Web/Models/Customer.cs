9a278c620d022e48ce025814d4165a888ff962cf
ashishyadav85805 <ashishyadav85805@gmail.com>
2022-08-16 10:58:32 +0530

    Add project files.

---
+++ Hotel.Web/Models/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+
+namespace Hotel.Web.Models
+{
+    [Table(name: "Customers")]
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "{0} cannot be empty!")]
+        [Column(TypeName = "varchar(50)")]
+        [Display(Name = "Name of the Customer")]
+
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage ="{0} cannot be empty")]
+        [Column(TypeName = "varchar(100)")]
+        [Display(Name = "Address")]
+
+        public string Address { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        [Display(Name ="Phone Number")]
+        public string PhoneNumber { get; set; }
+
+
+
+        
+
+        #region Navigation Properties to the OrderDetail Model
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+        #endregion
+    }
+}
